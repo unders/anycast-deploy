55a25ebd0ac2e87e529934bdf1c872fe6c9bc874
Anders Törnqvist <anders.tornqvist@gmail.com>
2019-04-16 18:14:04 +0200

    Add deploy script.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node
 LABEL maintainer="anders@functionbox.io"
 
-RUN npm i -g firebase-tools
+RUN npm i -g firebase-tools && apt-get update && apt-get install tree
 ADD /bin/deploy.sh /usr/bin
 RUN chmod +x /usr/bin/deploy.sh
 
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,11 @@
 main() {
     echo "running ./deploy.sh"
 
+    echo "Token" ${FIREBASE_TOKEN}
+    echo "PROJECT" ${FIREBASE_PROJECT}
+    tree
+    pwd
+
     # firebase deploy --token "$FIREBASE_TOKEN"
     firebase list --token ${FIREBASE_TOKEN} --project ${FIREBASE_PROJECT}
 }
